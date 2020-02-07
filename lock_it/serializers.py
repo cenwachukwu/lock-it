ca1cd268d6b13378b76b7ef6f00aef1ffd0a3ef6
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-07 17:29:14 -0500

    setting up views page

--- lock_it/serializers.py
+++ lock_it/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import UserAccount, Notes,
 
 
-# subscribedlist serializer
+# NotesSerializer serializer
 class NotesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notes
