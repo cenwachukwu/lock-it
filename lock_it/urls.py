64390dc55fbcce8454eea36717988a096e823a78
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-07 17:45:02 -0500

    created a view for note detail

---
+++ lock_it/urls.py
@@ -0,0 +1,2 @@
+from django.urls import path, include
+from views import notes_detail_view
--- lock_it/views.py
+++ lock_it/views.py
@@ -6,3 +6,15 @@ from .models import Notes, UserAccount
 from .serializers import NotesSerializer
 
 # Create your views here.
+# get notes detail view
+@api_view(['GET',])
+def notes_detail_view(request, slug):
+    try:
+        notes = Notes.objects.get(slug=slug)
+    except Notes.DoesNotExist:
+        # use response when returning responses from the rest framework
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "Get"
+        # here we are passing in a notes object into the NotesSerializer and returning the serialized data 
+        serializer = NotesSerializer(notes)
+        return Response(serializer.data)
